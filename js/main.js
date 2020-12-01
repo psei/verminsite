a8f72628ecb6bce46631d10474fc58666495e9d5
unknown <sprigge2013@gmail.com>
2020-12-01 22:18:09 +0100

    add image slider

--- js/main.js
+++ js/main.js
@@ -8,19 +8,38 @@ const dividerElements = document.querySelectorAll('.divider');
 const tapes = document.querySelectorAll('.tape');
 const tapeBtns = document.querySelectorAll('.tape-btn');
 
+// image slider
+const imageSlider = document.querySelector('.image-slider');
+const imagesContainer = imageSlider.querySelector('.image-slider__images');
+const sliderImages = imageSlider.querySelectorAll('.image-slider__image');
+const sliderDots = imageSlider.querySelectorAll('.dot');
+const nextBtn = imageSlider.querySelector('.next');
+const prevBtn = imageSlider.querySelector('.prev');
+const imageCount = imagesContainer.querySelectorAll('.image-slider__image').length - 1;
+let currentImage = 0;
+let imageSliderID;
+let startAgainID;
+const imageSliderInterval = 5000;
 
 ////////////////////////
 // event listeners
 function eventListeners() {
   // handle scroll event
   window.addEventListener('scroll', handleScroll);
-  //change language
+  // change language
   langBtns.forEach(btn => {
     btn.addEventListener('click', changeLangauge);
   });
-  //show tape content
+  // show tape content
   tapes.forEach(tape => {
     tape.addEventListener('click', showTapeContent);
+  });
+  // image slider controls
+  nextBtn.addEventListener('click', imageSliderControl);
+  prevBtn.addEventListener('click', imageSliderControl);
+  // image slider nav
+  sliderDots.forEach(dot => {
+    dot.addEventListener('click', imageSliderControl);
   });
 };
 
@@ -34,6 +53,55 @@ function handleScroll() {
 
 ////////////////////////
 // functions
+
+// image slider
+function imageSliderControl(e) {
+  if (e.target.classList.contains('prev')) {
+    if (currentImage - 1 < 0) {
+      currentImage = imageCount;
+    } else {
+      currentImage -= 1;
+    }
+    slideImage(currentImage);
+  } else if (e.target.classList.contains('next')) {
+    if (currentImage + 1 > imageCount) {
+      currentImage = 0;
+    } else {
+      currentImage += 1;
+    }
+    slideImage(currentImage);
+  } else if (e.target.classList.contains('dot')) {
+    currentImage = parseInt(e.target.dataset.image);
+    slideImage(currentImage);
+  }
+  clearInterval(imageSliderID);
+  startSlider();
+}
+
+function slideImage(slideTo) {
+  imagesContainer.style.transform = `translateX(-${slideTo*100}%)`;
+  // switch dots
+  sliderDots.forEach(dot => {
+    dot.classList.remove('active');
+  });
+  sliderDots[slideTo].classList.add('active');
+  // show overlay
+    sliderImages.forEach(image => {
+    image.classList.remove('active');
+  });
+  sliderImages[slideTo].classList.add('active');
+}
+
+function startSlider() {
+  imageSliderID = setInterval(() => {
+    if (currentImage + 1 > imageCount) {
+      currentImage = 0;
+    } else {
+      currentImage += 1;
+    }
+    slideImage(currentImage);
+  }, imageSliderInterval);
+}
 
 // show element when it is in viewport
 function handleDivider() {
@@ -146,6 +214,7 @@ function init() {
   hideLanguage('de');
   smoothScrool();
   eventListeners();
+  startSlider();
 };
 
 ////////////////////////
